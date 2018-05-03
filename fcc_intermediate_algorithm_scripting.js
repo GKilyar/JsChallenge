aab8e3da24fe4b07808e97560bae0a80d4b81278
ilyar <zz77zzgkilyar@163.com>
2018-05-03 19:48:12 +0800

    update

--- fcc_intermediate_algorithm_scripting.js
+++ fcc_intermediate_algorithm_scripting.js
@@ -81,7 +81,7 @@
 //     });
 //     return a1.concat(a2);
 //  }
- 
+
 //  diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 
 //No.3 给定数字转换成罗马数字 这个办法真的太牛皮了 想到了就无敌
@@ -119,7 +119,7 @@
 //         });
 //     });
 //   }
-  
+
 //   where([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
 
 
@@ -149,7 +149,7 @@
 //     arr.splice(num,0,after);
 //     return arr.join(' ');
 //   }
-  
+
 // myReplace("Let us go to the store", "store", "mall");
 
 //No.6 把一个英文单词的第一个辅音或辅音丛（consonant cluster）移到词尾，然后加上后缀 "ay"。
@@ -157,7 +157,7 @@
 // function translate(str) {
 //     var mystr = '';
 //     var regex = /[aeiou]/gi;
-    
+
 //     if(str[0].match(regex)){
 //       myStr = str+'way';
 //     }else{
@@ -165,8 +165,89 @@
 //       var index = str.indexOf(str.match(regex)[0]);
 //       myStr = str.substr(index) + str.substring(0,index) + 'ay';
 //     }
-    
+
 //     return myStr;
 //   }
-  
+
 //   translate("consonant");
+
+// No.7 convert HTML转换 字符串中的符号 RegExp
+// 将字符串中的字符 &、<、>、" （双引号）, 以及 ' （单引号）转换为它们对应的 HTML 实体。
+// 方法一
+// function convert(str){
+//     str = str.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/"/g,"quot;").replace(/'/g,"apos;")
+//     return str
+// }
+// convert("Dolce & Gabbana")
+// 方法二 放入对象中
+// function convert(str){
+//     var htmlEntities = {
+//         '&':'&amp;',
+//         '<':'&lt;',
+//         '>':'&gt;',
+//         '\"':'&quot;',
+//         '\'':'&apos;'
+//     }
+
+//     return str.split('').map(function(entry){
+//         return htmlEntities[entry] || entry
+//     }).join('')
+// }
+// convert("Dolce & Gabbana")
+//方法三 放入数组中
+// function convert(str) {
+//     var arr = ["&", "<", ">",'"', "'"];
+//     var fuArr = ["&amp;", "&lt;", "&gt;", "&quot;", "&apos;"];
+//     // 用正则表达式截取字符串中的字符
+//     var reg = /[&<>"']/g;
+//     var ssArr = str.match(reg);
+//     if (ssArr) {
+//         for (var i = 0; i < ssArr.length; i++) {
+//             var index = arr.indexOf(ssArr[i]);
+//             str = str.replace(ssArr[i], fuArr[index]);
+//         }
+//     }
+//     return str;
+// }
+
+// Node.8 将字符串中的空格下划线大小写 改为加入 - 
+// 例如 spinalCase("This Is Spinal Tap") 应该返回 "this-is-spinal-tap"。
+//      spinalCase("thisIsSpinalTap") 应该返回 "this-is-spinal-tap"。
+// spinalCase("The_Andy_Griffith_Show") 应该返回 "the-andy-griffith-show"。
+// spinalCase("Teletubbies say Eh-oh") 应该返回 "teletubbies-say-eh-oh"。
+
+// function spinalCase(str) {
+//     // 1. 正则表达式提取纯文本
+//     str = str.replace('-', ' ');
+//     str = str.replace(/[\W\^_]/g, ' ');
+//     var arr = str.split('');
+
+//     // 2. 根据大写分割为单词
+//     var sss = '';
+//     arr.map(function (val, index, arr) {
+//         var cindex = val.charCodeAt(0);
+//         if (cindex < 65 + 26) {
+//             // 如果是大写，在前面加个空格，用来分割
+//             sss += " " + val;
+//         } else {
+//             sss += val;
+//         }
+//     });
+
+//     // 3. 通过空格来进行分割
+//     // 3. 删除两端的空格
+//     sss = sss.trim();
+//     var arr2 = sss.split(' ');
+//     // 4.转为小写
+//     arr2 = arr2.map(function (val) {
+//         return val.toLowerCase();
+//     });
+
+//     arr2 = arr2.filter(function(val){
+//         return val !== "";
+//     });
+//     // 4.拼接，返回
+//     return arr2.join('-');
+// }
+
+// spinalCase('This Is Spinal Tap');
